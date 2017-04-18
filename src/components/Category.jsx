5038d1dd4450a902b22a869e07f6ca2fe64ef020
Kateryna Porkhun <admin@test.com>
2017-04-18 22:43:54 +0300

    feat(routing): add functionality for categories and tasks

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -5,67 +5,20 @@ class Category extends React.Component {
 
   constructor(props) {
     super(props);
-
-    this.state = {
-      isOpenModal: false
-    };
-
-  }
-
-  openEditModalWindow(categoryId, categoryName, action) {
-
-    this.props.changeCategory({
-      categoryChange: {
-        isModalOpen: true,
-        categoryId,
-        categoryName,
-        action
-      }
-    });
   }
 
   render() {
-    const subCategoriesData = this.props.data.subCategories;
-    let subCategoriesItems = '';
-
-    if(subCategoriesData) {
-
-      subCategoriesItems = subCategoriesData.map((item, i) => {
-        return (
-          <div className="category" key = {i}>
-              <div>
-                <Link to={`/category/${item.id}`}>{item.title}</Link>
-                <button className="category__edit" onClick={() => {this.openEditModalWindow(item.id, item.title, 'edit');}}></button>
-              </div>
-              <div>
-                <button className="category__del">&#10060;</button>
-                <button className="category__add" onClick={() => {this.openEditModalWindow(item.id, '', 'add');}}>+</button>
-              </div>
-          </div>
-
-        );
-      });
-    }
-
     return (
-      <div>
-        <div className="category">
-          <div>
-            <Link to={`/category/${this.props.data.id}`}>{this.props.data.title}</Link>
-            <button
-              className="category__edit"
-              onClick={() => {this.openEditModalWindow(this.props.data.id, this.props.data.title, 'edit');}}></button>
-          </div>
-          <div>
-            <button className="category__del">&#10060;</button>
-            <button
-              className="category__add"
-              onClick={() => {this.openEditModalWindow(this.props.data.id, '', 'add');}}>+</button>
-          </div>
+      <div className="category">
+        <div>
+          <Link to={`/category/${this.props.data.id}`}>{this.props.data.title}</Link>
+          <button className="category__edit"></button>
         </div>
-        {subCategoriesItems.length ? (<div className="sub-category">{subCategoriesItems}</div>) : ''}
+        <div>
+          <button className="category__del">&#10060;</button>
+          <button className="category__add">+</button>
+        </div>
       </div>
-
     )
   }
 }
--- src/containers/TodoList.jsx
+++ src/containers/TodoList.jsx
@@ -6,15 +6,58 @@ import Search from '../components/Header/Search.jsx';
 import ProgressBar from '../components/Header/ProgressBar.jsx';
 import AddItem from '../components/Header/AddItem.jsx';
 import CategoryList from './CategoryList.jsx'
+import TaskList from './TaskList.jsx'
+
+import { getCategory } from '../utils';
+
+import Modal from 'react-modal';
 
 const history = createBrowserHistory();
 class TodoList extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {categoryCounter: 6, categories: this.props.config.categories};
+    this.state = {
+      categoryCounter: 6,
+      categories: this.props.config.categories
+    };
+    this.data = this.props.config.categories;
 
-    this.getNewCategory = this.getNewCategory.bind(this);
+
+    this.saveCategoryChanges = this.saveCategoryChanges.bind(this);
+    this.saveTaskChanges = this.saveTaskChanges.bind(this);
+    this.changeCategory = this.changeCategory.bind(this);
+  }
+
+  changeCategory(categoryChange) {
+    this.setState(categoryChange);
+  }
+
+  saveCategoryChanges() {
+    const categoryState = this.state.categoryChange;
+    const currentCategory = getCategory(categoryState.categoryId, this.data);
+
+    if(categoryState.action == 'edit') {
+      currentCategory.title=this.modalCatField.value;
+    }
+    else {
+      currentCategory.subCategories.unshift(
+        {
+          id: `${categoryState.categoryId}.${currentCategory.subCategories.length + 1}`,
+          isActive: false,
+          isDone: false,
+          title: this.modalCatField.value,
+          subCategories: [],
+          tasks: []
+        }
+      )
+    }
+  }
+
+  saveTaskChanges(task, data, categoryParam, history) {
+    Object.assign(task, data);
+
+    history.push(`/category/${categoryParam}`);
   }
 
   getNewCategory(value) {
@@ -39,7 +82,6 @@ class TodoList extends React.Component {
   }
 
   render() {
-    const _this = this;
 
     return (
       <Router history = {history}>
@@ -51,10 +93,79 @@ class TodoList extends React.Component {
             </div>
             <ProgressBar />
           </div>
-          <Route path="/" component= {({ params } ) => {
-            return <CategoryList data = {_this.state.categories} getNewCategory = {_this.getNewCategory}/>
-          }} />
+          <AddItem getNewCategory = {this.getNewCategory} placeholder="Enter category title"/>
+          <div className="main">
+            <Route path="/" component= {({ params } ) => {
+                return <CategoryList
+                data = {this.data}
+                getNewCategory = {this.getNewCategory}/>
+              }}
+            />
+
+            <Route path="/category/:category" component={({ match } ) => {
+                const categoryParam = match.params.category;
+                const tasksData = getCategory(categoryParam, this.data).tasks;
+
+                return <TaskList data = {tasksData} category={categoryParam} />
+              }}
+            />
+            <Route path="/category/:category/edit" component={({ match } ) => {
+                const categoryParam = match.params.category;
+                const category = getCategory(categoryParam, this.data);
+
+                return <Modal
+                  className="modal-category"
+                  isOpen={true}
+                  contentLabel="Modal"
+                  >
+                  <Link to={`/category/${categoryParam}`}>&#10060;</Link>
+                  <h3>Category name:</h3>
+                  <input type="text" ref={(input) => { this.modalCatField = input; }} defaultValue={category.title}/>
+                  <button onClick={this.saveCategoryChanges}>Save</button>
+                </Modal>
+              }}
+              />
+
+            <Route path="/category/:category/task/:task" component={({ match, history } ) => {
+              const categoryParam = match.params.category;
+              const taskParam = match.params.task;
+
+              const tasksData = getCategory(categoryParam, this.data).tasks;
+              const task = tasksData.filter(item => {
+                return item.id === taskParam;
+              })[0];
+
+              return <Modal
+                          className="modal-category"
+                          isOpen={true}
+                          contentLabel="Modal"
+                          >
+                          <Link to={`/category/${categoryParam}`}>&#10060;</Link>
+                          <button onClick={() => {
+                            this.saveTaskChanges(task,
+                            {
+                            title: this.taskTitle.value,
+                            description: this.taskDescription.value
+                            },
+                            categoryParam,
+                            history);
+                          }}>Save changes</button>
+                          <Link to={`/category/${categoryParam}`}>
+                            <button>Cancel</button>
+                          </Link>
+                          <div>
+                            <input type="text" ref={(input) => {this.taskTitle = input; }} defaultValue={task.title}/>
+                            <label><input type="checkbox"/> Done</label>
+                            <textarea cols="30" rows="10" ref={(input) => {this.taskDescription = input; }} defaultValue={task.description}></textarea>
+                          </div>
+
+                        </Modal>
+              }}
+            />
+          </div>
         </div>
+
+
       </Router>
     )
   }
